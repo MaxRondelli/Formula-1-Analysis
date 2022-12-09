1ca5c0af0b25b1c67128be6a365c8042f9aa45ae
MaxRondelli <83310259+MaxRondelli@users.noreply.github.com>
2022-12-09 19:05:24 +0100

    Update telemetry_acceleration.py
    Now it's possible to compute the telemetry for two driver in order to see the differences between them.
    
    Lateral Acceleration is available only for Verstappen on his fastest lap during quali in Imola Grand Prix 2022.

--- telemetry_acceleration.py
+++ telemetry_acceleration.py
@@ -49,64 +49,149 @@ if count == 3:
     count = 4
     session.load()  # Load the session
 
-# -------------- DRIVER --------------
+# -------------- DRIVER 1 --------------
 print("\nChoose one of the following driver.")
 print(session.results['Abbreviation'])
 while count == 4:
-    driver = input("\nWrite the driver:")
+    driver_1 = input("\nWrite the driver 1:")
     for i in session.results['Abbreviation']:
-        if i == driver:
-            laps_driver = session.laps.pick_driver(driver)
-            fastest_driver = laps_driver.pick_fastest()
-            telemetry_driver = fastest_driver.get_telemetry()
-            count = -1
+        if i == driver_1:
+            laps_driver_1 = session.laps.pick_driver(driver_1) 
+            fastest_driver_1 = laps_driver_1.pick_fastest() 
+            telemetry_driver_1 = fastest_driver_1.get_telemetry() 
+
+            count = 5
 
     if count == 4:
         print("\nDriver's abbreviation incorrect. Try again.")
+        
+# -------------- DRIVER 2 --------------
+print("\nChoose one of the following driver.")
+print(session.results['Abbreviation'])
+while count == 5:
+    driver_2 = input("\nWrite the driver 2:")
+    for i in session.results['Abbreviation']:
+        if i == driver_2:
+            laps_driver_2 = session.laps.pick_driver(driver_2) 
+            fastest_driver_2 = laps_driver_2.pick_fastest() 
+            telemetry_driver_2 = fastest_driver_2.get_telemetry() 
 
-# Team color for plot
-team_driver = laps_driver['Team'].iloc[0]
-color = ff1.plotting.team_color(team_driver)
+            count = -1
 
-# Set the size of the plot
+    if count == 5:
+        print("\nDriver's abbreviation incorrect. Try again.")
+        
+# Variables for the plots
+team_driver_1 = fastest_driver_1['Team']
+team_driver_2 = fastest_driver_2['Team']
+
+driver_speed_1 = telemetry_driver_1['Speed']
+driver_speed_2 = telemetry_driver_2['Speed']
+
+driver_throttle_1 = telemetry_driver_1['Throttle']
+driver_throttle_2 = telemetry_driver_2['Throttle']
+
+driver_brake_1 = telemetry_driver_1['Brake']
+driver_brake_2 = telemetry_driver_2['Brake']
+
+driver_gear_1 = telemetry_driver_1['nGear']
+driver_gear_2 = telemetry_driver_2['nGear']
+
+driver_distance_1 = telemetry_driver_1['Distance']
+driver_distance_2 = telemetry_driver_2['Distance']
+
+# Plot configuration
+plot_size = [15,15]
+plot_title = f"{session.event.year} {session.event.EventName} - {session.name} - {driver_1}"
+plot_ratios = [1, 3, 2, 1, 1, 2, 1]
+plot_filename = plot_title.replace(" ", " ") + ".png"
+
 plt.rcParams['figure.figsize'] = [20, 15]
-fig, ax = plt.subplots(3, gridspec_kw={'height_ratios': [5, 5, 5]})
+fig, ax = plt.subplots(7, gridspec_kw={'height_ratios': [5, 5, 5, 5, 5, 7, 7]})
 
 # Subplot 1: Speed
-ax[0].plot(telemetry_driver['Distance'], telemetry_driver['Speed'], label=driver, color = color)
-ax[0].set(ylabel="Speed")
+ax[0].plot(telemetry_driver_1['Distance'], telemetry_driver_1['Speed'], label=driver_1, color = ff1.plotting.team_color(team_driver_1))
+ax[0].plot(telemetry_driver_2['Distance'], telemetry_driver_2['Speed'], label=driver_2, color = ff1.plotting.team_color(team_driver_2))
+ax[0].set(ylabel='Speed')
 ax[0].legend(loc="lower right")
 
-driver_speed = telemetry_driver['Speed']
-driver_throttle = telemetry_driver['Throttle']
-driver_brake = telemetry_driver['Brake']
-driver_distance = telemetry_driver['Distance']
-driver_gear = telemetry_driver['nGear']
+# Subplot 2: Throttle
+ax[1].plot(telemetry_driver_1['Distance'], telemetry_driver_1['Throttle'], label=driver_1, color=ff1.plotting.team_color(team_driver_1))
+ax[1].plot(telemetry_driver_2['Distance'], telemetry_driver_2['Throttle'], label=driver_2, color=ff1.plotting.team_color(team_driver_2))
+ax[1].set(ylabel='Throttle')
+
+# Subplot 3: Brake
+ax[2].plot(telemetry_driver_1['Distance'], telemetry_driver_1['Brake'], label=driver_1, color=ff1.plotting.team_color(team_driver_1))
+ax[2].plot(telemetry_driver_2['Distance'], telemetry_driver_2['Brake'], label=driver_2, color=ff1.plotting.team_color(team_driver_2))
+ax[2].set(ylabel='Brake')
+
+# Subplot 4: Gear
+ax[3].plot(telemetry_driver_1['Distance'], telemetry_driver_1['nGear'], label=driver_1, color=ff1.plotting.team_color(team_driver_1))
+ax[3].plot(telemetry_driver_2['Distance'], telemetry_driver_2['nGear'], label=driver_2, color=ff1.plotting.team_color(team_driver_2))
+ax[3].set(ylabel='Gear')
+
+# Subplot 5: RPM
+ax[4].plot(telemetry_driver_1['Distance'], telemetry_driver_1['RPM'], label=driver_1, color=ff1.plotting.team_color(team_driver_1))
+ax[4].plot(telemetry_driver_2['Distance'], telemetry_driver_2['RPM'], label=driver_2, color=ff1.plotting.team_color(team_driver_2))
+ax[4].set(ylabel='RPM')
 
 def longAcceleration():
-    driver_time = (telemetry_driver['Time'] / np.timedelta64(1, 's')).astype(int)
-    driver_speed = telemetry_driver['Speed']
-    raw_acceleration_data = []
+    driver_time_1 = (telemetry_driver_1['Time'] / np.timedelta64(1, 's')).astype(int)
+    driver_time_2 = (telemetry_driver_2['Time'] / np.timedelta64(1, 's')).astype(int)
+    
+    driver_speed_1 = telemetry_driver_1['Speed']
+    driver_speed_2 = telemetry_driver_2['Speed']
+    
+    raw_acceleration_data_1 = []
+    raw_acceleration_data_2 = []
 
-    for i in range(0, driver_time.size):
+    for i in range(0, driver_time_1.size):
 
-        s_p = driver_speed.iloc[i-1]            # Previous speed
-        s_f = driver_speed.iloc[i]              # Current speed
-        t_p = driver_time.iloc[i-1]             # Previous time
-        t_f = driver_time.iloc[i]               # Current time
+        s_p = driver_speed_1.iloc[i-1]            # Previous speed
+        s_f = driver_speed_1.iloc[i]              # Current speed
+        t_p = driver_time_1.iloc[i-1]             # Previous time
+        t_f = driver_time_1.iloc[i]               # Current time
 
         with np.errstate(divide='ignore', invalid = 'ignore'):
             acc = ((s_f - s_p) / 3.6) / (t_f - t_p)
-            raw_acceleration_data.append(acc)
+            raw_acceleration_data_1.append(acc)
 
-        acceleration_data = [v for v in raw_acceleration_data if not (math.isinf(v) or math.isnan(v) or v < -6.0)]
+        acceleration_data_1 = [v for v in raw_acceleration_data_1 if not (math.isinf(v) or math.isnan(v) or v < -6.0)]
 
-    # Subplot 2: Longitudinal Acceleration
-    ax[1].plot(acceleration_data, label="LEC", color = color)
-    ax[1].set_ylabel("Long Acceleration")
+    for i in range(0, driver_time_2.size):
+        
+            s_p = driver_speed_2.iloc[i-1]            # Previous speed
+            s_f = driver_speed_2.iloc[i]              # Current speed
+            t_p = driver_time_2.iloc[i-1]             # Previous time
+            t_f = driver_time_2.iloc[i]               # Current time
 
+            with np.errstate(divide='ignore', invalid = 'ignore'):
+                acc = ((s_f - s_p) / 3.6) / (t_f - t_p)
+                raw_acceleration_data_2.append(acc)
+
+            acceleration_data_2 = [v for v in raw_acceleration_data_2 if not (math.isinf(v) or math.isnan(v) or v < -6.0)]
+
+    # Subplot 5: Longitudinal Acceleration
+    ax[5].plot(acceleration_data_1, label = driver_1, color = ff1.plotting.team_color(team_driver_1))
+    ax[5].plot(acceleration_data_2, label = driver_2, color = ff1.plotting.team_color(team_driver_2))
+    ax[5].set_ylabel("Long Acc")
+
+'''
+Lateral Acceleration is avaible only for Verstappen during Quali in Imola 2022.
+'''
 def latAcceleration():
-    if grand_prix == "Imola" and driver == "VER":
+    if grand_prix == "Imola" and driver_1 == "VER" or driver_2 == "VER":
+        
+        driver = "VER"
+        laps_driver = session.laps.pick_driver(driver) 
+        fastest_driver = laps_driver.pick_fastest() 
+        telemetry_driver = fastest_driver.get_telemetry() 
+        team_driver = fastest_driver['Team']
+
+        driver_distance = telemetry_driver['Distance']
+        driver_gear = telemetry_driver['nGear']
+        driver_speed = telemetry_driver['Speed']
+
         excel_data = pd.read_excel('Dataset/data_imola_circuit.xlsx') # Load xlsx file
         df = pd.DataFrame(excel_data) # Read the values of the file in the dataframe
 
@@ -126,13 +211,14 @@ def latAcceleration():
                             g_force = acceleration / 9.81
                             raw_acceleration_data.append(g_force)    
 
-                        acceleration_data = [v for v in raw_acceleration_data if not (math.isinf(v) or math.isnan(v) or v > 7.0 or v == 0)]                                                      
+                        acceleration_data = [v for v in raw_acceleration_data if not (math.isinf(v) or math.isnan(v) or v > 7.0 or v == 0)]                                               
         
         print("\n", acceleration_data)
-        # Subplot 3: Lateral Acceleration
-        ax[2].plot(acceleration_data, label=driver, color = color)
-        ax[2].set(ylabel='Lateral Acceleration')
-           
+        
+        # Subplot 6: Lateral Acceleration
+        ax[6].plot(acceleration_data, label = driver, color = ff1.plotting.team_color(team_driver))
+        ax[6].set(ylabel='Lat Acc')
+        
 longAcceleration()
 latAcceleration()
 plt.show()
